795881f753e2f4d1a892ab175431889b7af1277b
Divyansh Kushwaha <kushwaha.divyansh.dxn@gmail.com>
2021-12-06 23:20:35 +0530

    initial scaffolding and dependencies setup

---
+++ android/app/src/main/java/com/dxn/connectingaspirants/common/Result.kt
@@ -0,0 +1,7 @@
+package com.dxn.connectingaspirants.common
+
+sealed class Result<T>(val data: T?, val message:String?) {
+    class Success<T>(data: T):Result<T>(data,null)
+    class Failure<T>(message: String):Result<T>(null,message)
+    class Loading<T>(message: String?):Result<T>(null,message)
+}
